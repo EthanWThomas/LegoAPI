141189c9f7ad02414afb08481eb190c2e1b39fdf
EthanWThomas <111320646+EthanWThomas@users.noreply.github.com>
2024-08-06 17:10:49 -0600

    Initial Commit

---
+++ LegoAPI/LegoAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LegoAPIApp.swift
+//  LegoAPI
+//
+//  Created by Ethan Thomas on 8/6/24.
+//
+
+import SwiftUI
+
+@main
+struct LegoAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
